624ab00785059244f9478ba71ca9c44a0600c59c
SidZhoop <siddharth@zhoop.in>
2023-09-11 00:08:12 +0530

    navbar completed

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,76 @@
+import { useState } from "react";
+
+const Navbar = () => {
+  const [nav, setNav] = useState(false);
+
+  const handleNav = () => {
+    setNav(!nav);
+  };
+  return (
+    <div className="flex justify-around h-24 max-w-[1240px] mx-auto px-4 items-center text-white">
+      <h1 className="w-full text-3xl font-bold text-[#00df9a]">Company</h1>
+      <ul className="hidden md:flex">
+        <li className="p-4">Home</li>
+        <li className="p-4">Company</li>
+        <li className="p-4">Resources</li>
+        <li className="p-4">About</li>
+        <li className="p-4">Contact</li>
+      </ul>
+
+      <div className="cursor-pointer block md:hidden" onClick={handleNav}>
+        {!nav ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg>
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+            />
+          </svg>
+        )}
+      </div>
+
+      <div
+        className={
+          !nav
+            ? "fixed left-0 top-0 w-[60%] border-r border-r-gray-900 h-full bg-[#000300] ease-in-out duration-500 md:hidden"
+            : "fixed left-[-100%]"
+        }
+      >
+        <h1 className="w-full m-4 text-3xl font-bold text-[#00df9a]">
+          Company
+        </h1>
+        <ul className="p-4 uppercase">
+          <li className="p-4 border-b border-gray-600">Home</li>
+          <li className="p-4 border-b border-gray-600">Company</li>
+          <li className="p-4 border-b border-gray-600">Resources</li>
+          <li className="p-4 border-b border-gray-600">About</li>
+          <li className="p-4">Contact</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
